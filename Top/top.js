2f08caaf87690cbbb36d847064185b305752847a
Bruyin <chlacchy96@gmail.com>
2026-03-19 14:05:10 +0100

    Gestito header comune

--- Top/top.js
+++ Top/top.js
@@ -1,13 +1,16 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navMenu = document.getElementById('nav-menu');
-
-menuToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-});
-
-// chiudi il menu se l'utente clicca fuori
-document.addEventListener('click', (event) => {
+fetch('../Top/header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header-placeholder').innerHTML = data;
+    // IMPORTANTE: Reinizializza il menu a tendina qui dentro dopo il caricamento
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.getElementById('nav-menu');
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+    document.addEventListener('click', (event) => {
     if (!menuToggle.contains(event.target) && !navMenu.contains(event.target)) {
         navMenu.classList.remove('active');
     }
 });
+  });
